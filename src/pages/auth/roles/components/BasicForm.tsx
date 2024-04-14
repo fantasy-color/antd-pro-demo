96fc4fb71c60bb45c847b7ef84648bf352aacac0
韩毅 <12379330+hanyi319@user.noreply.gitee.com>
2024-04-14 16:18:50 +0800

    refactor: 使用 useForm 重构表单

--- src/pages/auth/roles/components/BasicForm.tsx
+++ src/pages/auth/roles/components/BasicForm.tsx
@@ -1,11 +1,50 @@
+import useQueryList from '@/hooks/useQueryList';
 import { ProForm, ProFormText } from '@ant-design/pro-components';
-import React from 'react';
+import { TreeProps, Tree, FormInstance, Form } from 'antd';
+import React, { Key, useState } from 'react';
 
 interface Props {
-  newRecord?: boolean;
+  form: FormInstance<any>;
 }
 
-const BasicForm: React.FC<Props> = () => {
+const BasicForm: React.FC<Props> = (props) => {
+  const { form } = props;
+  const [expandedKeys, setExpandedKeys] = useState<Key[]>([]);
+  const [checkedKeys, setCheckedKeys] = useState<Key[]>([]);
+  const [selectedKeys, setSelectedKeys] = useState<Key[]>([]);
+  const [autoExpandParent, setAutoExpandParent] = useState<boolean>(true);
+
+  const { items: permissionGroups } = useQueryList('/permission_groups');
+
+  const onExpand: TreeProps['onExpand'] = (expandedKeysValue) => {
+    console.log('onExpand', expandedKeysValue);
+    // if not set autoExpandParent to false, if children expanded, parent can not collapse.
+    // or, you can remove all expanded children keys.
+    setExpandedKeys(expandedKeysValue);
+    setAutoExpandParent(false);
+  };
+
+  const onCheck: TreeProps['onCheck'] = (checkedKeysValue) => {
+    console.log('onCheck', checkedKeysValue);
+    setCheckedKeys(checkedKeysValue as Key[]);
+
+    const permissions = (checkedKeysValue as Key[]).filter((key: Key) =>
+      key.toString().startsWith('permission'),
+    );
+    const permissionIds = permissions.map((key: Key) =>
+      Number(key.toString().replace('permission-', '')),
+    );
+
+    form.setFieldsValue({
+      permissionIds,
+    });
+  };
+
+  const onSelect: TreeProps['onSelect'] = (selectedKeysValue, info) => {
+    console.log('onSelect', info);
+    setSelectedKeys(selectedKeysValue);
+  };
+
   return (
     <>
       <ProForm.Group>
@@ -15,6 +54,20 @@ const BasicForm: React.FC<Props> = () => {
           label="名称"
           name="name"
         />
+        <Form.Item name="permissionIds">
+          <div>选择权限</div>
+          <Tree
+            checkable
+            onExpand={onExpand}
+            expandedKeys={expandedKeys}
+            autoExpandParent={autoExpandParent}
+            onCheck={onCheck}
+            checkedKeys={checkedKeys}
+            onSelect={onSelect}
+            selectedKeys={selectedKeys}
+            treeData={permissionGroups}
+          />
+        </Form.Item>
       </ProForm.Group>
     </>
   );
--- src/pages/auth/roles/components/CreateForm.tsx
+++ src/pages/auth/roles/components/CreateForm.tsx
@@ -1,7 +1,7 @@
-import { ModalForm, ProForm, ProFormText } from '@ant-design/pro-components';
-import React, { Key, useState } from 'react';
-import { Tree, TreeProps } from 'antd';
-import useQueryList from '@/hooks/useQueryList';
+import { ModalForm } from '@ant-design/pro-components';
+import React from 'react';
+import BasicForm from './BasicForm';
+import { Form } from 'antd';
 
 interface Props {
   open: boolean;
@@ -11,73 +11,22 @@ interface Props {
 
 const Create: React.FC<Props> = (props) => {
   const { open, onOpenChange, onFinish } = props;
-  const [expandedKeys, setExpandedKeys] = useState<Key[]>([]);
-  const [checkedKeys, setCheckedKeys] = useState<Key[]>([]);
-  const [selectedKeys, setSelectedKeys] = useState<Key[]>([]);
-  const [autoExpandParent, setAutoExpandParent] = useState<boolean>(true);
-
-  const { items: permissionGroups } = useQueryList('/permission_groups');
-
-  const onExpand: TreeProps['onExpand'] = (expandedKeysValue) => {
-    console.log('onExpand', expandedKeysValue);
-    // if not set autoExpandParent to false, if children expanded, parent can not collapse.
-    // or, you can remove all expanded children keys.
-    setExpandedKeys(expandedKeysValue);
-    setAutoExpandParent(false);
-  };
-
-  const onCheck: TreeProps['onCheck'] = (checkedKeysValue) => {
-    console.log('onCheck', checkedKeysValue);
-    setCheckedKeys(checkedKeysValue as Key[]);
-  };
-
-  const onSelect: TreeProps['onSelect'] = (selectedKeysValue, info) => {
-    console.log('onSelect', info);
-    setSelectedKeys(selectedKeysValue);
-  };
+  const [form] = Form.useForm();
 
   return (
     <ModalForm
+      form={form}
       title="新增角色"
       width="740px"
       open={open}
       onOpenChange={onOpenChange}
-      onFinish={async (values: any) => {
-        const permissions = (checkedKeys as Key[]).filter((key: Key) =>
-          key.toString().startsWith('permission'),
-        );
-        const permissionIds = permissions.map((key: Key) =>
-          Number(key.toString().replace('permission-', '')),
-        );
-        await onFinish({ ...values, permissionIds: permissionIds });
-      }}
+      onFinish={onFinish}
       modalProps={{
         destroyOnClose: true,
         maskClosable: false,
       }}
     >
-      <ProForm.Group>
-        <ProFormText
-          rules={[{ required: true, message: '请输入名称' }]}
-          width="md"
-          label="名称"
-          name="name"
-        />
-        <div>
-          <div>选择权限</div>
-          <Tree
-            checkable
-            onExpand={onExpand}
-            expandedKeys={expandedKeys}
-            autoExpandParent={autoExpandParent}
-            onCheck={onCheck}
-            checkedKeys={checkedKeys}
-            onSelect={onSelect}
-            selectedKeys={selectedKeys}
-            treeData={permissionGroups}
-          />
-        </div>
-      </ProForm.Group>
+      <BasicForm form={form} />
     </ModalForm>
   );
 };
